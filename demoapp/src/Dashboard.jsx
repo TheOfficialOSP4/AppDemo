582155cf181f4cd970d96f49c0105a578649ae39
sifulsidd <sifulsiddiki@gmail.com>
2023-07-18 21:07:15 -0400

    updated one more thing on dashboard

--- demoapp/src/Dashboard.jsx
+++ demoapp/src/Dashboard.jsx
@@ -1,11 +1,17 @@
 // import
 import './dashboard.css';
 // import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Dashboard() {
+  const navigate = useNavigate();
+  const click = () => {
+    return navigate('/archive');
+  };
+
   return (
     <div>
-      Welcome
+      <button onClick={click}>CLICK ME!</button>
     </div>
   )
 }
